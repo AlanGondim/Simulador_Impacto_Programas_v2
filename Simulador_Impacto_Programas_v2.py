68968775fef08adcc12a2865f8c9026bcdd53c7b
Alan Gondim <143657788+AlanGondim@users.noreply.github.com>
2026-03-26 08:21:26 -0300

    Update Simulador_Impacto_Programas_v2.py

--- Simulador_Impacto_Programas_v2.py
+++ Simulador_Impacto_Programas_v2.py
@@ -107,7 +107,7 @@ local_css()
 
 st.markdown(f"""
 <div class="header-box">
-    <h2 style="margin:0; color:#003366;">📑 Relatório PMO PROGRAMAS - Análise de Impacto Financeiro</h2>
+    <h2 style="margin:0; color:#003366;">📑 Relatório de Análise de Impacto Financeiro - PMO Programas</h2>
 </div>
 """, unsafe_allow_html=True)
 
