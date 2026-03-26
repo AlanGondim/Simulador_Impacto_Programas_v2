5f5479cc5eba5894abdc03781c036d7d9e8168ec
Alan Gondim <143657788+AlanGondim@users.noreply.github.com>
2026-03-26 14:46:56 -0300

    Update Simulador_Impacto_Programas_v2.py

--- Simulador_Impacto_Programas_v2.py
+++ Simulador_Impacto_Programas_v2.py
@@ -256,7 +256,7 @@ with col_g1:
 st.markdown('<div class="section-header">7. DRE do Programa: Análise de margem final</div>', unsafe_allow_html=True)
 with st.container(border=True):
     d1, d2, d3 = st.columns(3)
-    margem_meta = d1.number_input("Margem inicial (Meta) %", value=35.0)
+    margem_meta = d1.number_input("Margem inicial (Meta) %", value=45.0, Step=1.0)
     receita_liq = d2.number_input("Receita líquida atual", min_value=1.0 , value=5000.0, step=1000.0)
     custo_eac_atual = d3.number_input("Custo total atual (EAC)", min_value=1.0, value=1000.0, step=1000.0)
 
